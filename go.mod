b816d18660583dea5fd46947a18a230211fc0474
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-01-19 18:48:01 -0500

    jwt middleware done

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/bmdavis419/golang-auth0-example
 
 go 1.19
 
-require github.com/gofiber/fiber/v2 v2.41.0
+require (
+	github.com/auth0/go-jwt-middleware/v2 v2.1.0
+	github.com/gofiber/fiber/v2 v2.41.0
+	github.com/spf13/viper v1.15.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/auth0/go-jwt-middleware/v2 v2.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
@@ -22,13 +25,14 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.15.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.43.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -2,7 +2,13 @@ package auth
 
 import (
 	"fmt"
+	"log"
+	"net/url"
+	"strings"
+	"time"
 
+	"github.com/auth0/go-jwt-middleware/v2/jwks"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
 	"github.com/bmdavis419/golang-auth0-example/config"
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,10 +24,42 @@ func NewAuthMiddleware(config config.EnvVars) *AuthMiddleware {
 }
 
 func (a *AuthMiddleware) ValidateToken(c *fiber.Ctx) error {
-	// Set a custom header on all responses:
-	c.Set("X-Custom-Header", "Hello, World")
+	issuerURL, err := url.Parse("https://" + a.config.AUTH0_DOMAIN + "/")
+	if err != nil {
+		log.Fatalf("Failed to parse the issuer url: %v", err)
+	}
 
-	fmt.Println("hello", a.config.AUTH0_DOMAIN)
+	provider := jwks.NewCachingProvider(issuerURL, 5*time.Minute)
+
+	jwtValidator, err := validator.New(
+		provider.KeyFunc,
+		validator.RS256,
+		issuerURL.String(),
+		[]string{a.config.AUTH0_AUDIENCE},
+	)
+	if err != nil {
+		log.Fatalf("Failed to set up the jwt validator")
+	}
+
+	// get the token from the request header
+	authHeader := c.Get("Authorization")
+	authHeaderParts := strings.Split(authHeader, " ")
+	if len(authHeaderParts) != 2 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Invalid authorization header",
+		})
+	}
+
+	// Validate the token
+	tokenInfo, err := jwtValidator.ValidateToken(c.Context(), authHeaderParts[1])
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Invalid token",
+		})
+	}
+
+	fmt.Println(tokenInfo)
 
 	// Go to next middleware:
 	return c.Next()
